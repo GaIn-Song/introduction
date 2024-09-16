1dd610eeb319797c409664d7f65e99890380f07d
Ga In <ll21324@bristol.ac.uk>
2024-09-16 15:43:23 +0100

    final final commit

--- intro.py
+++ intro.py
@@ -1,9 +1,13 @@
 ## Introduction 
 
-print("Hello World!")
+print("Hello Worldg!")
 
 if __name__ == "__main__":
     print("Made a change")
     print("Made another change")
     print("Changed something else!")
+
+    # Generate a random number between 1 and 10 and print it
+    import random
+    print(random.randint(1, 10))
     
---
+++ intro_v2.py
@@ -0,0 +1,3 @@
+# New file
+
+print("New program")
