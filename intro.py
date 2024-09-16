79849123ad9aada703b0806983eebfff3cc04172
Ga In <ll21324@bristol.ac.uk>
2024-09-16 15:43:07 +0100

    final commit

--- intro.py
+++ intro.py
@@ -1,8 +1,9 @@
 ## Introduction 
 
-print("Hello World")
+print("Hello World!")
 
 if __name__ == "__main__":
-    print("My name is Ga In")
     print("Made a change")
     print("Made another change")
+    print("Changed something else!")
+    
